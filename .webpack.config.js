6a9aa60aea59c1654877f34bb26415bf813473ec
dhole <dhole.me@gmail.com>
2020-04-28 23:30:02 +0800

    no msg

--- .webpack.config.js
+++ .webpack.config.js
@@ -1,5 +1,8 @@
 module.exports = config => {
     console.log('TARGET: ', process.env.TARGET);
     console.log('NODE_ENV: ', process.env.NODE_ENV);
+    if(process.env.NODE_ENV === 'production'){
+        config.output.publicPath = "static/";
+    }
     return config;
 }
